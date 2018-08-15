5c8ef1836c3c83d6dc0da24649bd5760ce1687b0
Dan Codae <drcoda@gmail.com>
2018-08-15 14:47:24 -0400

    Tina figured out the dates

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 console.log('Write your code here!')
 console.log('customers and moment are available to you.')
+
+// Consider making a Customer class that takes one customer's data (one element of the `customers` array). Some methods you could implement:
+
+//   * `getName()` - returns the full name, capitalized correctly
+//   * `getAddress()` - returns the address, formatted correctly
+//   * `makeDOMNode()` - return the entire DOM node, probably a `div`, for displaying the customer
+
+class Employee {
+    constructor(name, startDate, birthDate, address, thumbnailPhoto, email) {
+        // this.name = name;
+        // this.startDate = startDate;
+        // this.address = address;
+        // this.birthDate = birthDate;
+        // this.thumbnailPhoto = thumbnailPhoto;
+    }
+
+    getName() {
+        return this.name(" ")[1, 2]
+    }
+
+    getAddress(){
+    }
+    getPhoneNumber() {
+    }
+    getThumbnailPhoto() {
+    }
+    getCustomerSince() => {
+    return moment(registered).format('MMM Do, YYYY')
+    }
+    getEmail() {
+    }
+    getBirthDate() => {
+    return moment(dob).format('MMM Do, YYYY')
+    }
+
+
+var phoenix = new Employee("Phoenix Carter",
+    new Date())
